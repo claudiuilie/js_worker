c4e4d4507733357be905b393fe23c08a703ef735
claudiuilie <42173289+claudiuilie@users.noreply.github.com>
2020-03-16 21:13:14 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,7 @@
 class Options {
     constructor() {
         this.mysql = {
-            host: "192.168.1.102",
-            user: "prodapp",
-            password: "Bulgaria188",
-            database: "home_app"
+
         }
     }
 }
